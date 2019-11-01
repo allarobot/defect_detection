0c864bc14582cef239ede18b5092613f230cd1db
JH <jay.cn@outlook.com>
2019-11-02 01:04:14 +0800

    blob detection
    1. detection function
    2. blob register
    3.blob monitor

---
+++ src/blobdetection/blob_monitor.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+"""
+monitor blob variation with a reference points data
+this is a runtime operation code
+"""
+
+import cv2
+import numpy as np;
+import pickle
+import blob
+
+fileName = "data/BlobTest2b.jpg"
+kpt = "keypoints"
+
+# Load blobs points
+file = open(kpt, "rb")
+pnts_ref = pickle.load(file)
+file.close()
+
+
+def monitor(pnts1,pnts2):
+    keypoints = []
+    for pt, size in pnts1:
+        keypoint = cv2.KeyPoint(pt[0], pt[1], size)
+        overlap = False
+        for pnt in pnts2:
+            val = cv2.KeyPoint_overlap(pnt,keypoint)
+            if val > 0.5:
+                overlap = True
+                continue
+        if not overlap:
+            keypoints.append(keypoint)
+    return keypoints
+
+
+# Read image
+im = cv2.imread(fileName, cv2.IMREAD_GRAYSCALE)
+
+# Detect blobs.
+keypoints,im_with_keypoints = blob.detection(im)
+
+#
+pnts = monitor(pnts_ref,keypoints)
+
+# Draw detected blobs as red circles.
+# cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
+im_with_keypoints = cv2.drawKeypoints(im, pnts, np.array([]), (0, 0, 255),
+                                      cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+# Show keypoints
+cv2.imshow("Keypoints", im_with_keypoints)
+cv2.waitKey(0)
